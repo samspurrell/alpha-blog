932eb6091cfb74d73a4dd2deb89c73b74b6b3639
Sam Spurrell <samspurrell@gmail.com>
2016-03-20 19:50:20 +0000

    add assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-    validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+    
+    has_many :articles
+    before_save { self.email = email.downcase}
+    
+    validates :username, presence: true, 
+                length: { minimum: 3, maximum: 25 }, 
+                uniqueness: { case_sensitive: false }
     
     VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
